5fe5a6043b2adc5393bcc79275971165408405e0
cloudzhao <wlsdhzy@163.com>
2018-05-24 14:51:29 +0800

    调整新接口

--- service-restful/deploy/sy_app_manager.sh
+++ service-restful/deploy/sy_app_manager.sh
@@ -4,6 +4,10 @@ base=$(cd $(dirname $0);pwd)
 test -f $HOME/.bash_profile && . $HOME/.bash_profile
 
 function get_abs_file(){
+	test ! -f $1 && {
+		echo $1;
+		return;
+	}
 	local d=$(cd $(dirname $1);pwd)
 	echo "$d/$(basename $1)"
 }
@@ -17,6 +21,14 @@ function curl_cmd(){
 		echo "curl command not found." >&2;
 		return;
 	}
+	if [ "x$method" == "xUPLOAD" ];then
+		test -f "$data" && {
+			echo "$_curl_cmd -F f1=@$(get_abs_file $data) $url";
+            $_curl_cmd -F f1=@$(get_abs_file $data) $url;
+            echo
+        }
+        return
+    fi
 	test "x$data" != "x" && data="-d $data"
 	local curl_param="-s -X $method -H Content-Type:application/json"
 	echo "$_curl_cmd $curl_param $url $data"
@@ -24,16 +36,34 @@ function curl_cmd(){
 	echo
 }
 
-function submit(){
+function upload(){
 	test "x$1" == "x" && return
-	local data='{"conf":"'$(get_abs_file $1)'"}'
+
+	curl_cmd "$http_server/naja/upload" "UPLOAD" "$1"
+}
+
+function app_json(){
+	test $# -ne 3 && return
+	echo '{"packageName":"'$1'","conf":"'$(get_abs_file $2)'","decompression":'$3'}'
+}
+
+function once(){
+	test $# -ne 3 && return
+	local data='{"app":'$(app_json $1 $2 $3)'}'
+
+	curl_cmd "$http_server/app/execute" "POST" "$data"
+}
+
+function submit(){
+	test $# -ne 3 && return
+	local data=$(app_json "$@")
 
 	curl_cmd "$http_server/app/submit" "POST" "$data"
 }
 
 function scheduled(){
-	test $# -ne 2 && return
-	local data='{"conf":"'$(get_abs_file $1)'","interval":"'$2'"}'
+	test $# -ne 4 && return
+	local data='{"app":'$(app_json $1 $2 $3)',"interval":'$4'}'
 
 	curl_cmd "$http_server/app/scheduled" "POST" "$data"
 }
@@ -64,11 +94,14 @@ function getMonitors(){
 
 function printHelp(){
 	echo  "Usage: $0 -h server_ip:port parameter"
-	echo -e "Parameter:\n\tsubmit conf"
-	echo -e "\tscheduled conf interval"
+	echo -e "Parameter:"
+	echo -e "\tupload file"
+	echo -e "\tonce package conf decompress"
+	echo -e "\tsubmit package conf decompress"
+	echo -e "\tscheduled pakcage conf decompress interval"
 	echo -e "\tkill appid"
 	echo -e "\tmonitors app_name"
-	echo -e "\theartbeat appid period"
+	echo -e "\theartbeat app_name appid period"
 }
 
 function _check_(){
@@ -90,17 +123,39 @@ function main(){
 				http_server=${args[i+1]}
 				_check_ $http_server
 				let i=$i+1;;
-			"submit")
-				submit_conf=${args[i+1]}
+			"upload")
+				file=${args[i+1]}
+				_check_ $file
+				upload $file
+				exit;;
+			"once")
+				package=${args[i+1]}
+				submit_conf=${args[i+2]}
+				decompress=${args[i+3]}
+				_check_ $package
 				_check_ $submit_conf
-				submit $submit_conf
+				_check_ $decompress
+				once $package $submit_conf $decompress
+				exit;;
+			"submit")
+				package=${args[i+1]}
+				submit_conf=${args[i+2]}
+				decompress=${args[i+3]}
+				_check_ $package
+				_check_ $submit_conf
+				_check_ $decompress
+				submit $package $submit_conf $decompress
 				exit;;
 			"scheduled")
-				submit_conf=${args[i+1]}
-				interval_time=${args[i+2]}
+				package=${args[i+1]}
+				submit_conf=${args[i+2]}
+				decompress=${args[i+3]}
+				interval_time=${args[i+4]}
+				_check_ $package
 				_check_ $submit_conf
+				_check_ $decompress
 				_check_ $interval_time
-				scheduled $submit_conf $interval_time
+				scheduled $package $submit_conf $decompress $interval_time
 				exit;;
 			"kill")
 				app_id=${args[i+1]}
@@ -112,10 +167,11 @@ function main(){
 				getMonitors $app_name
 				exit;;
 			"heartbeat")
-				app_id=${args[i+1]}
-				period=${args[i+2]}
+				app_name=${args[i+1]}
+				app_id=${args[i+2]}
+				period=${args[i+3]}
 				_check_ $app_id
-				sendHeartbeat $app_id $period
+				sendHeartbeat $app_name $app_id $period
 				exit;;
 			*)
 				printHelp
